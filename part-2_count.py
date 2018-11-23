a980f811ae37885a1abc316b18b815d67809d6cd
Teresawidjaja <37266622+Teresawidjaja@users.noreply.github.com>
2018-11-23 15:26:55 +0800

    Update part-2_count.py

--- part-2_count.py
+++ part-2_count.py
@@ -1,48 +1,46 @@
 
-f = open ("train","r",encoding="utf8")
-lines = f.readlines()
+def word_sentiment_count(file):
+    f = open (file,"r",encoding="utf8")
+    lines = f.readlines()
 
-list_words= [] #a list of tweet_notation
-for i in range(len(lines)): #: range(30) 
-    if (lines[i]!=" "):      
+    list_words= [] #a list of tweet_notation
+    for i in range(len(lines)): #: range(30) 
+        if (lines[i]!=" "):      
 
-        lines[i]= lines[i].replace("\n","")                       
-        lines[i] =lines[i].split(" ") #word[i] = each object in the list, in a form of a list 'tweet','notation'
+            lines[i]= lines[i].replace("\n","")                       
+            lines[i] =lines[i].split(" ") #word[i] = each object in the list, in a form of a list 'tweet','notation'
 
-    if (lines[i]!=[""] and lines[i]!=['.','O'] and lines[i]!=["","O"] and lines[i][1]!="" and lines[i]!=['.'] and lines[i][1]!=".") :
-           list_words.append(lines[i]) #deletes lines with nothing
+        if (lines[i]!=[""] and lines[i]!=['.','O'] and lines[i]!=["","O"] and lines[i][1]!="" and lines[i]!=['.'] and lines[i][1]!=".") :
+               list_words.append(lines[i]) #deletes lines with nothing
 
-#keyerror: key does not exist   (obj requested but key doesnt exist)         
+    #keyerror: key does not exist   (obj requested but key doesnt exist)         
 
-wordcount_dict={}
-# print (list_words)
+    wordcount_dict={}
+    print (list_words, lines[i])
 
-for j in range(len(list_words)): #len(list_words)
-    word = list_words[j][0]
-    word=word.lower()
-    sentiment= list_words[j][1]
-    #sentiment_count={}
-    
-    if word not in wordcount_dict:
-        sentiment_count={sentiment:1}
-        wordcount_dict[word]=sentiment_count
-        
-    else: #word is already a key in wordcount_dict
-        if sentiment not in word:
-            sentiment_count[sentiment]=1
-            wordcount_dict[word]=sentiment_count
-        else:
-            sentiment_count = wordcount_dict[word]
-            print (sentiment_count)
-            sentiment_count[sentiment]+=1
+    for j in range(len(list_words)): #len(list_words)
+        word = list_words[j][0]
+        word=word.lower()
+        sentiment= list_words[j][1]
+       
+
+        if word not in wordcount_dict:
+            sentiment_count={sentiment:1}
             wordcount_dict[word]=sentiment_count
 
-            
-try:            
-    print(wordcount_dict)
-except KeyError:
-    print(lines[i])
-        
+        else: #word is already a key in wordcount_dict
+            if sentiment not in word:
+                sentiment_count[sentiment]=1
+                wordcount_dict[word]=sentiment_count
+            else:
+                sentiment_count = wordcount_dict[word]
+                print (sentiment_count)
+                sentiment_count[sentiment]+=1
+                wordcount_dict[word]=sentiment_count
+
+    return (wordcount_dict)
+
+print (word_sentiment_count("train"))
         
 #print (list_words)
 
