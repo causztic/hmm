bd493d976012fece17be0b9694316606fc0a21da
yaojie <limyaojie93@gmail.com>
2018-11-24 11:50:01 +0800

    renamed variables to be consistent

--- src/part4.py
+++ src/part4.py
@@ -3,13 +3,13 @@ import part2
 
 def viterbi(sentence, X, S, Y, A, B):
     """
-    This algorithm generates a path which is a sequence of states that generates the observations.
+    This algorithm generates a path which is a sequence of labels that generates the observations.
     The code references the pseudocode from Wikipedia https://en.wikipedia.org/wiki/Viterbi_algorithm.
     It is modified to suit our previous codes.
 
     We define these symbols:
     X => the set of observations. There are N unique observations, and T observations in this particular sentence.
-    S => the set of states. There are K unique states, and T states in this particular sentence.
+    S => the set of labels. There are K unique labels, and T labels in this particular sentence.
 
     other inputs are:
     sentence => the sentence we are analyzing. it is T-long.
@@ -45,7 +45,7 @@ def viterbi(sentence, X, S, Y, A, B):
 
     last_values = [T1[k, T-1] for k in range(K)]
     Z[T-1] = last_values.index(np.amax(last_values)) # find the k index responsible for largest value.
-    result[T-1] = S[Z[T-1]] # get the optimal state by index.
+    result[T-1] = S[Z[T-1]] # get the optimal label by index.
 
     for i in range(T-1, 0, -1): # from the 2nd last item to the first item.
         Z[i-1] = T2[Z[i], i]
@@ -95,7 +95,7 @@ def viterbi_2(sentence, X, S, Y, A, B):
 
     last_values = [T1[k, T-1] for k in range(K)]
     Z[T-1] = last_values.index(np.amax(last_values)) # find the k index responsible for largest value.
-    result[T-1] = S[Z[T-1]] # get the optimal state by index.
+    result[T-1] = S[Z[T-1]] # get the optimal label by index.
 
     for i in range(T-1, 0, -1): # from the 2nd last item to the first item.
         Z[i-1] = T2[Z[i], i]
@@ -107,22 +107,22 @@ if __name__ == "__main__":
     for locale in ["EN", "FR"]:
         DATA = open(f"./../data/{locale}/train")
         training_set = part2.prepare_data(DATA)
-        _results, words, label_counts, emission_counts = part2.estimate_emissions(
+        _results, observations, label_counts, emission_counts = part2.estimate_emissions(
             training_set)
 
         TEST_DATA = open(f"./../data/{locale}/dev.in")
         testing_set = part2.prepare_data(TEST_DATA)
         # with the test data, we are able to smooth out the emissions.
         results = part2.smooth_emissions(
-            testing_set, words, label_counts, emission_counts)
+            testing_set, observations, label_counts, emission_counts)
 
         # from the results, we generate a K x N matrix.
         B = np.zeros((len(label_counts), len(results)))
         for j, (j_key, j_value) in enumerate(results.items()):
             for i, i_key in enumerate(label_counts.keys()):
                 if i_key in j_value:
-                    # if the state emits the word at j
+                    # if the label emits the observation at j
                     B[i, j] = j_value[i_key]
         print(f"S -> {label_counts.keys()}")
-        print(f"X -> {len(results)} words")
+        print(f"X -> {len(results)} observations")
         print(B.shape)
